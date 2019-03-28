602da9fad6c7e7883ead6c7672765e1184fae0d0
VeronicaEms <38732906+VeronicaEms@users.noreply.github.com>
2019-03-28 10:43:09 -0300

    Create web-server.js

---
+++ services/web-server.js
@@ -0,0 +1,52 @@
+const http = require('http');
+const morgan = require('morgan');
+const express = require('express');
+const webServerConfig = require('../config/web-server.js');
+ 
+let httpServer;
+ 
+function initialize() {
+  return new Promise((resolve, reject) => {
+    const app = express();
+    httpServer = http.createServer(app);
+ 
+    
+     // Combines logging info from request and response
+     app.use(morgan('combined'));
+ 
+     // *** app.get call below this line ***
+
+    app.get('/', (req, res) => {
+      res.end('Hello World!');
+    });
+ 
+    httpServer.listen(webServerConfig.port)
+      .on('listening', () => {
+        console.log(`Web server listening on localhost:${webServerConfig.port}`);
+ 
+        resolve();
+      })
+      .on('error', err => {
+        reject(err);
+      });
+  });
+}
+ 
+module.exports.initialize = initialize;
+
+// *** previous code above this line ***
+ 
+function close() {
+  return new Promise((resolve, reject) => {
+    httpServer.close((err) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+ 
+      resolve();
+    });
+  });
+}
+ 
+module.exports.close = close;
