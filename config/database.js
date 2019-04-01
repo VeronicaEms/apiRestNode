102d8502915b1bb50218d0c0c1d11829ed0d2109
VeronicaEms <38732906+VeronicaEms@users.noreply.github.com>
2019-04-01 09:06:21 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,15 +1,10 @@
-/*
-este arquivo permite que algumas propriedades sejam configuradas através de variáveis ​​de ambiente
-ajuda a manter as senhas e outras informações confidenciais fora do código-fonte
-*/
-
 module.exports = {
-    hrPool: {
-      user: process.env.HR_USER,
-      password: process.env.HR_PASSWORD,
-      connectString: process.env.HR_CONNECTIONSTRING,
-      poolMin: 10,
-      poolMax: 10,
-      poolIncrement: 0
-    }
-  };
+  hrPool: {
+  user: "********",
+  password: "**********",
+  connectString: "(DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = amzp.***.com.br)(PORT = ***))) (CONNECT_DATA = (SERVICE_NAME = ***)))",
+  poolMin: 10,
+  poolMax: 10,
+  poolIncrement: 0
+  }
+};
