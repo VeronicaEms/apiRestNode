dcb3e84fb2acdf732605b1b72b6c524247cc0bf7
VeronicaEms <38732906+VeronicaEms@users.noreply.github.com>
2019-04-01 09:20:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,34 +4,33 @@ const webServer = require('./services/web-server.js');
 //  *** line that requires services/web-server.js is here ***
 const database = require('./services/database.js');
 
-
-// *** line that requires services/web-server.js is here ***
-const dbConfig = require('./config/database.js');
-const defaultThreadPoolSize = 4;
  
-
-// Increase thread pool size by poolMax
-process.env.UV_THREADPOOL_SIZE = dbConfig.hrPool.poolMax + defaultThreadPoolSize;
+/* O módulo principal traz o módulo do servidor da web e, em seguida,
+ define e chama uma função assíncrona denominada inicialização
+Como a função initialize do módulo do servidor web retorna uma promessa,
+você pode usá-lo com async/await e envolvê-lo em um bloco try-catch,
+Se a função de inicialização terminar com sucesso, o servidor da Web estará em execução;
+caso contrário, quaisquer exceções serão capturadas e tratadas
  
+ */
 async function startup() {
   console.log('Starting application');
 
   try {
-    console.log('Initializing database module');
+    console.log('Initializing web server module');
  
-    await database.initialize(); 
+    await webServer.initialize();
   } catch (err) {
     console.error(err);
  
     process.exit(1); // Non-zero failure code
   }
   
-  // *** existing try block in startup here ***
  
   try {
-    console.log('Initializing web server module');
+    console.log('Initializing database module');
  
-    await webServer.initialize();
+    await database.initialize(); 
   } catch (err) {
     console.error(err);
  
@@ -43,16 +42,17 @@ startup();
 
 // *** previous code above this line ***
  
+ 
 async function shutdown(e) {
   let err = e;
     
   console.log('Shutting down');
  
   try {
-    console.log('Closing web server module');
+    console.log('Closing database module');
  
-    await webServer.close();
-  } catch (e) {
+    await database.close(); 
+  } catch (err) {
     console.log('Encountered error', e);
  
     err = err || e;
