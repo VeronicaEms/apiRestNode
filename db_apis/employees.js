a8f62797c485a888d73f876b96cd26683b4f5c22
VeronicaEms <38732906+VeronicaEms@users.noreply.github.com>
2019-04-11 10:42:32 -0300

    Update employees.js

--- db_apis/employees.js
+++ db_apis/employees.js
@@ -1,4 +1,5 @@
 const database = require('../services/database.js');
+const oracledb = require('oracledb');
  
 /* O módulo baseQuery traz o módulo de banco de dados genérico e inicializa uma 
 constante denominada baseQuery para uma consulta SQL na tabela employees. 
@@ -6,11 +7,8 @@ Em seguida, uma função chamada find é declarada e usada para executar a consu
 Se o parâmetro de contexto transmitido tiver um valor de id "geral",
 uma cláusula where será anexada à consulta, de modo que apenas um único funcionário seja retornado.
 */
-const baseQuery = 
- `select id_pessoa "id_pessoa",
-    nome "nome"
-  from fn_pessoa `;
- 
+const baseQuery = `select id_pessoa, nome, apelido from rjs_pessoa`;
+
   async function find(context) {
     let query = baseQuery;
     const binds = {};
@@ -27,3 +25,30 @@ const baseQuery =
 }
  
 module.exports.find = find;
+
+
+//Uma constante chamada createSql foi criada para inserção dos dados na tabela. 
+const createSql = `insert into rjs_pessoa (id_pessoa, nome, apelido) values (srjs_pessoa.nextval, :nome, :apelido) returning id_pessoa into :id_pessoa`;
+
+ /*
+Dentro da função create, uma constante "emp" é definida e inicializada
+
+*/
+ async function create(emp) {
+   const employee =  {
+    nome: emp.nome,
+    apelido: emp.apelido,
+    id_pessoa: {
+      type: oracledb.NUMBER,
+      dir: oracledb.BIND_OUT
+    }
+  }
+ 
+
+   const result = await database.simpleExecute(createSql, employee);
+   //employee = result.outbinds.employee[0];
+   //return employee;
+   return result;
+ }
+
+ module.exports.create = create;
