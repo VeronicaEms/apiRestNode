06d38483ef547927e728e576886076692bc4a32a
VeronicaEms <38732906+VeronicaEms@users.noreply.github.com>
2019-04-18 11:49:08 -0300

    Create middleware.js

---
+++ services/middleware.js
@@ -0,0 +1,37 @@
+let jwt = require('jsonwebtoken');
+const config = require('../config/database.js');
+
+/*
+x-acess-token e a header que o back-end envia apos a verificação do token
+bearer autenticação, pois ele trafega um token
+slice pega a string apartir da posição especifica
+*/
+let checkToken = (req, res, next) => {
+  let token = req.headers['x-access-token'] || req.headers['authorization'];
+  if (token.startsWith('Bearer')) {
+    token = token.slice(7, token.length);
+  }
+
+  if (token) {
+    jwt.verify(token, config.secret, (err, decoded) => {
+      if (err) {
+        return res.json({
+          success: false,
+          message: 'Token não é válido'
+        });
+      } else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    return res.json({
+      success: false,
+      message: 'O token de autenticação não foi fornecido'
+    });
+  }
+};
+
+module.exports = {
+  checkToken: checkToken
+}
