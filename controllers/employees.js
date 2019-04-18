9c23cb0b147abbf239c3dde1be5f615ce422d302
VeronicaEms <38732906+VeronicaEms@users.noreply.github.com>
2019-04-18 11:47:03 -0300

    Update employees.js

--- controllers/employees.js
+++ controllers/employees.js
@@ -1,6 +1,6 @@
 const employees = require('../db_apis/employees.js'); // a api do banco de dados
- 
-
+const bcrypt = require('bcryptjs');
+const salt = bcrypt.genSaltSync(10);
 /* Uma função assíncrona chamada get é declarada. Um bloco try-catch
 é usado no corpo da função para capturar exceções lançadas
 no thread principal e passá-las para a próxima função. 
@@ -43,21 +43,20 @@ Solicitações HTTP POST são usadas para criar novos registros (neste caso).
 A função getEmployeeFromRec aceita um objeto de solicitação e retorna um objeto
 com as propriedades necessárias para criar um registro.
 A função foi declarada fora da função post para que possa ser usada posteriormente para solicitações PUT.*/
-
-
 function getEmployeeFromRec(req) {
   const employee = {
-  //  id_pessoa: req.body.id_pessoa,
     nome: req.body.nome,
-    apelido: req.body.apelido
-  };
+    apelido: req.body.apelido,
+    email: req.body.email,
+    password: bcrypt.hashSync(req.body.password, salt)
+  }
 return employee;
 }
 
 
+
 /*A função post usa getEmployeeFromRec para inicializar uma variável
 que é então passada para o método de criação da API do banco de dados */
-
 
 async function post(req, res, next) {
  
@@ -71,7 +70,6 @@ juntamente com o JSON da pessoa (incluindo o novo valor de ID da pessoa), é env
  res.status(201).json(employee);
   } catch (err) {
     next(err);
-  }
-  }
+  }}
 
 module.exports.post = post;
