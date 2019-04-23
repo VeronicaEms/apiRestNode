c2c906c3e2a98b76fdce996cff69d49ca1ff6580
VeronicaEms <38732906+VeronicaEms@users.noreply.github.com>
2019-04-23 15:43:56 -0300

    Update middleware.js

--- services/middleware.js
+++ services/middleware.js
@@ -1,31 +1,39 @@
-let jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const config = require('../config/database.js');
 
 /*
-x-acess-token e a header que o back-end envia apos a verificação do token
+x-acess-token é a header que o back-end envia apos a verificação do token
 bearer autenticação, pois ele trafega um token
-slice pega a string apartir da posição especifica
+slice pega a string a partir da posição específica
 */
 let checkToken = (req, res, next) => {
-const token = req.header('Authorization').replace("Bearer ", '');
-  /*let token = req.headers['x-access-token'] || req.headers['authorization'];
+  /*let token = req.headers['x-access-token'] || req.headers['Authorization'];
   if (token.startsWith('Bearer')) {
-    token = token.slice(7, token.length);
-  }*/
-
-
+    token = token.slice(7, token.length); }*/
+    const token = req.header('Authorization').replace("Bearer ", '');
+  
+console.log(token);
   if (token) {
-    jwt.verify(token, config.secret, (err, decoded) => {
+    jwt.verify(token, config.hrPool.secret, (err, decoded) => {
+
       if (err) {
         return res.json({
           success: false,
-          message: 'Token não é válido'
+          message: 'Token não é válido',
+          token,
+          err     
         });
+
       } else {
         req.decoded = decoded;
-        next();
+       next();
+        /*return res.json({
+          success: true,
+          message: 'Token ok'     
+        });*/
       }
     });
+
   } else {
     return res.json({
       success: false,
