61b35f9af834dc7cf6c65a25418f81d2305e45ae
VeronicaEms <38732906+VeronicaEms@users.noreply.github.com>
2019-05-02 16:14:43 -0300

    Update employees.js

--- db_apis/employees.js
+++ db_apis/employees.js
@@ -18,7 +18,7 @@ const baseQuery = `select id_pessoa, nome, apelido, email, password from rjs_pes
  
     query += `\n where id_pessoa = :id_pessoa`;
   }
-  console.log(employee);
+ 
   const result = await database.simpleExecute(query, binds);
  
   return result.rows;
@@ -27,7 +27,7 @@ const baseQuery = `select id_pessoa, nome, apelido, email, password from rjs_pes
 module.exports.find = find;
 
 
-//Uma constante chamada createSql foi criada para inserção dos dados na tabela. 
+//Uma constante chamada createSql foi criada para inserção dos dados na tabela do banco.
 const createSql = `insert into rjs_pessoa (id_pessoa, nome, apelido, email, password)
 values (srjs_pessoa.nextval, :nome, :apelido, :email, :password) returning id_pessoa into :id_pessoa`;
 
@@ -55,4 +55,36 @@ values (srjs_pessoa.nextval, :nome, :apelido, :email, :password) returning id_pe
 
  module.exports.create = create;
 
+ 
+ async function getAll() {
+  const query = `select id_pessoa, nome, apelido, email from rjs_pessoa`;
+ const result = await database.simpleExecute(query,{});
+ return result.rows;
+} 
+module.exports.getAll = getAll;
 
+
+//Essa função edita os dados da tabela do frontend
+async function putAll(binds) {
+  const updateQuery = `UPDATE rjs_pessoa SET nome = :nome, apelido = :apelido, email = :email WHERE id_pessoa = :id_pessoa`;
+  console.log("> UPDATE:", updateQuery);
+  const result = await database.simpleExecute(updateQuery, binds);
+  console.log("> RESULT:", result.rowsAffected);
+  return result.rowsAffected;
+}
+module.exports.putAll = putAll;
+
+
+
+async function findOne(id) {
+  let query = `select id_pessoa, nome, apelido, email from rjs_pessoa`;
+  const binds = {};
+
+  if (id) {
+    query += ` where id_pessoa = ${id}`;
+  }
+  const result = await database.simpleExecute(query, binds);
+  return result;
+}
+
+module.exports.findOne = findOne;
