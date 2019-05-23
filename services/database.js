7bfa51a934ffdb0a56043421a9a34ff401ee419f
VeronicaEms <38732906+VeronicaEms@users.noreply.github.com>
2019-05-23 12:42:50 -0300

    Update database.js

--- services/database.js
+++ services/database.js
@@ -1,18 +1,18 @@
-const oracledb = require('oracledb');
-const dbConfig = require('../config/database.js');
- 
-
 /*
 Este módulo primeiro traz o node-oracledb e o arquivo de configuração.
 Em seguida, uma função assíncrona denominada initialize é definida e
 posteriormente exposta por meio do objeto module.exports.
-A função initialize cria um conjunto de conexões que é armazenado em um cache
-do conjunto de conexões internas como o pool “padrão”
+A função initialize cria um conjunto de conexões que é armazenado
+em um cache do conjunto de conexões internas como o pool “padrão”.
 */
-async function initialize() {
-  const pool = await oracledb.createPool(dbConfig.hrPool);
-}
+
+const oracledb = require('oracledb');
+const dbConfig = require('../config/database.js');
  
+async function initialize() {
+  await oracledb.createPool(dbConfig.hrPool);
+}
+
 module.exports.initialize = initialize;
 
 // *** previous code above this line ***
