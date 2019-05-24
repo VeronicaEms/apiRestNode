d2ef481751d3f522e69da7cb4eea01edd65cf1b1
VeronicaEms <38732906+VeronicaEms@users.noreply.github.com>
2019-05-24 14:15:10 -0300

    Update login.js

--- db_apis/login.js
+++ db_apis/login.js
@@ -5,21 +5,19 @@ async function find(context) {
   let query = `SELECT id_pessoa, password FROM rjs_pessoa`;
   const binds = {};
 
-  console.log("CONTEXT:", context);
+  console.log("\n>>> db_api/login.js (find) CONTEXT:\n", context);
 
   if (context.email && context.password) {
     binds.email = context.email;
-    //binds.password = context.password;
-    query += `\n WHERE email = :email`;
-    //query += `\n WHERE email = :email AND password =:password`;
+    query += ` WHERE email = :email`;
   }
 
-  console.log("QUERY SQL:", query, binds);
+  console.log("\n>>> db_api/login.js (QUERY SQL):\n", query, binds);
 
   // realiza a consulta na tabela, procurando apenas o usuário pelo email
   const result = await database.simpleExecute(query, binds);
 
-  console.log("RESULT QUERY:", result);
+  console.log("\n>>> db_api/login.js (RESULT QUERY):\n", result);
 
   // Se o result retornar um objecto com o resultado do ID e PASSWORD ele será maior que 0
   // que dizer que a consulta pelo email encontrou um usuário cadastrado, nessa condição
@@ -28,16 +26,17 @@ async function find(context) {
   if (result.rows.length > 0) {
     // hashpasswd = ele é o campo da tabela PASSWORD criptografada que faz a comparação da variavel enviada pelo JSON,
     // o campo hashpassword esta criptografado... e o campo context.password text/string
-    let hashpasswd = result.rows[0]['PASSWORD'];
-    let id_pessoa = result.rows[0]['ID_PESSOA'];
+    let hashpasswd = result.rows[0]["PASSWORD"];
+    let id_pessoa = result.rows[0]["ID_PESSOA"];
+
+    console.log(">>> id_pessoa", id_pessoa);
+    console.log(">>> hashpasswd", hashpasswd);
 
     // compara a senha informada no front-end com a senha cadastrada no banco de dados,
     // se forem iguais o "compare" retornará TRUE e retornará o ID_PESSOA para o controllers.
     // se as senhas não forem iguais o "compare" retorna FALSE, nesse caso o find retornará -1
     // para o controllers indicando que a senha não confere
     if (await bcrypt.compare(context.password, hashpasswd)) {
-      console.log("db_apis - RESULT SQL:", id_pessoa);
-
       return { id_pessoa: id_pessoa };
     } else {
       // Senhas diferentes
@@ -51,3 +50,4 @@ async function find(context) {
 }
 
 module.exports.find = find;
+
